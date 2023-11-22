795d047a5bb9aad1c3a1074a8538b65b4f868de8
ksan <aquam41@gmail.com>
2023-11-22 12:04:03 +0900

    project setting, test hibernate

---
+++ src/main/java/com/example/testapp/start/JpaMain.java
@@ -0,0 +1,51 @@
+package com.example.testapp.start;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void logic(EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("지한");
+        member.setAge(2);
+
+        em.persist(member);
+
+        member.setAge(20);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember : " + findMember.getUsername() + ", age: " + findMember.getAge());
+
+        List<Member> members = 
+            em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        System.out.println("member size : " + members.size());
+
+        // em.remove(member);
+    }
+}
